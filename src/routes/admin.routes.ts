458130bb66f50e444a96369089057b5789d146a5
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2024-05-17 18:58:21 +0200

    fix(routing): Fix admin routing

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import rolesRoutes from "../controllers/admin/roles.controller";
-import athletesRoutes from "../controllers/admin/roles.controller";
+import athletesRoutes from "../controllers/admin/athletes.controller";
+import countriesRoutes from "../controllers/admin/countries.controller";
 
 const router = express.Router();
 
 router.use("/roles", rolesRoutes);
 router.use("/athletes", athletesRoutes);
+router.use("/countries", countriesRoutes);
 
 export default router;
