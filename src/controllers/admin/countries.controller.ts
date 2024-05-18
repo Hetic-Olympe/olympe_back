471a0f0a55a628a8bb797f4ff2c8fce6456a51e0
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2024-05-18 16:32:12 +0200

    feat(Countries): Provide update route to update the participation country field

--- src/controllers/admin/countries.controller.ts
+++ src/controllers/admin/countries.controller.ts
@@ -12,4 +12,20 @@ router.get("/", async (req: Request, res: Response) => {
   }
 });
 
+router.post("/update", async (req: Request, res: Response) => {
+  try {
+    const countryId = req.body.id;
+    const country = await service.getCountryById(countryId);
+    if (country === null) {
+      throw new Error("Country not find");
+    }
+    const updateCountry = { ...country, isParticipate: !country.isParticipate };
+    console.log(updateCountry);
+    await service.updateCountry(updateCountry);
+    res.status(200).send({ success: "Country successfuly updated" });
+  } catch (error) {
+    res.status(500).send({ error: "An error occurred" });
+  }
+});
+
 export default router;
--- src/services/countries.service.ts
+++ src/services/countries.service.ts
@@ -5,12 +5,29 @@ const countryRepository = AppDataSource.getRepository(Country);
 
 export const getCountries = async (): Promise<Country[]> => {
   const countries = await countryRepository.find({
+    relations: ["continent"],
     select: {
       id: true,
       iso: true,
       nicename: true,
-      isParticpate: true,
+      isParticipate: true,
+      continent: {
+        name: true,
+      },
+    },
+    order: {
+      iso: "ASC",
     },
   });
   return countries;
+};
+
+export const updateCountry = async (country: Country): Promise<Country> => {
+  const updatedCountry = await countryRepository.save(country);
+  return updatedCountry;
+};
+
+export const getCountryById = async (id: number): Promise<Country | null> => {
+  const country = await countryRepository.findOneOrFail({ where: { id } });
+  return country;
 };
