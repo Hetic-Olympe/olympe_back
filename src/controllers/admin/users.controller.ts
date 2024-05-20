806d13317a79982921feb17a8aa3934e7b11f581
MaximePETIT-code <contactmaximepetit@gmail.com>
2024-05-20 22:29:40 +0200

    feat(admin-user-manager): get & patch

---
+++ src/controllers/admin/users.controller.ts
@@ -0,0 +1,38 @@
+import express, { Request, Response } from "express";
+import * as service from "../../services/users.service";
+import { excludeNonUpdatableFields } from "../../utils/excludeNonUpdatableFields";
+
+const router = express.Router();
+
+router.get("/", async (req: Request, res: Response) => {
+    try {
+        const users = await service.getUsers();
+        res.status(200).send(users);
+    } catch (error) {
+        res.status(500).send({ error: "An error occurred" });
+    }
+});
+
+router.get("/:id", async (req: Request, res: Response) => {
+    const id = req.params.id;
+    try {
+        const users = await service.getUserDetail(id);
+        res.status(200).send(users);
+    } catch (error) {
+        res.status(500).send({ error: "An error occurred" });
+    }
+});
+
+router.patch("/:id", async (req: Request, res: Response) => {
+    const id = req.params.id;
+    const fieldsToUpdate = excludeNonUpdatableFields(req.body, ['id', 'password', 'createdAt', 'isConnected']);
+    try {
+        const updatedUser = await service.updateUser(id, fieldsToUpdate);
+        res.status(200).send(updatedUser);
+    } catch (error) {
+        res.status(500).send({ error: "An error occurred while updating the user." });
+    }
+});
+
+
+export default router;
--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -3,9 +3,23 @@ import { User } from "../models/User";
 
 const userRepository = AppDataSource.getRepository(User);
 
-export const getUsers = async (): Promise<User[]> => {
-  const users = await userRepository.find();
-  return users;
+const FIELDS_TO_SELECT: (keyof User)[] = [
+  "id",
+  "email",
+  "firstname",
+  "lastname",
+  "phone",
+  "isConnected",
+  "createdAt"
+];
+
+const getUserWithRelations = async (conditions: any, relations: string[]): Promise<User | null> => {
+  const user = await userRepository.findOne({
+    where: conditions,
+    relations: relations,
+    select: FIELDS_TO_SELECT,
+  });
+  return user;
 };
 
 export const createUser = async (data: User): Promise<User> => {
@@ -13,16 +27,26 @@ export const createUser = async (data: User): Promise<User> => {
   return newUser;
 };
 
+export const getUsers = async (): Promise<User[]> => {
+  const users = await userRepository.find({
+    relations: ["role", "interests", "likes"],
+    select: FIELDS_TO_SELECT,
+  });
+  return users;
+};
+
+export const getUserDetail = async (id: string): Promise<User | null> => {
+  return await getUserWithRelations({ id }, ["role", "interests", "likes"]);
+};
+
+export const updateUser = async (id: string, fieldsToUpdate: Partial<User>): Promise<User | null> => {
+  await userRepository.update(id, fieldsToUpdate);
+  return await getUserWithRelations({ id }, []);
+};
+
+
 export const getUserByEmail = async (email: string): Promise<User | null> => {
-  try {
-    const user = await userRepository.findOneOrFail({
-      where: { email },
-      relations: ["role"],
-    });
-    return user;
-  } catch (error) {
-    return null;
-  }
+  return await getUserWithRelations({ email }, ["role"]);
 };
 
 export const getUserById = async (id: string): Promise<User | null> => {
