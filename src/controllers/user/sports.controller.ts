118fd3f277542991a6bc3f3656133b69c47870b4
MaximePETIT-code <contactmaximepetit@gmail.com>
2024-05-25 18:55:01 +0200

    fix(sportField): good /fields route

--- src/controllers/user/sports.controller.ts
+++ src/controllers/user/sports.controller.ts
@@ -12,20 +12,20 @@ router.get("/", async (req: Request, res: Response) => {
     }
 });
 
-router.get("/:id", async (req: Request, res: Response) => {
-    const { id } = req.params;
+router.get("/fields", async (req: Request, res: Response) => {
     try {
-        const sport = await service.getDetailSport(id);
-        res.status(200).send(sport);
+        const sportFields = await service.getSportFields();
+        res.status(200).send(sportFields);
     } catch (error) {
         res.status(500).send({ error: "An error occurred" });
     }
 });
 
-router.get("/fields", async (req: Request, res: Response) => {
+router.get("/:id", async (req: Request, res: Response) => {
+    const { id } = req.params;
     try {
-        const sportFields = await service.getSportFields();
-        res.status(200).send(sportFields);
+        const sport = await service.getDetailSport(id);
+        res.status(200).send(sport);
     } catch (error) {
         res.status(500).send({ error: "An error occurred" });
     }
